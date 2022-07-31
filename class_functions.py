3208df4ba621f67294edb37125f55fbb8239bfa8
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-31 21:58:31 +0530

    Add files via upload

---
+++ class_functions.py
@@ -0,0 +1,64 @@
+''' creating a normal class '''
+# class grow():
+#     frew=10
+#     dew=20.3
+    
+# print(grow.dew)                                             # 20.3
+# print(grow.frew)                                            # 10
+# w = grow()
+# print(w.dew)                                                # 20.3
+# print(w.frew)                                               # 10
+
+''' nested for loop '''
+# e = [
+#     [1,2,5],
+#     [12,45,63],
+#     [23,21.3,562.3]
+# ]
+# print(e)
+# print(e[0])                           # [1,2,5]
+# print(e[0][1])                        # 2
+# for n in e:
+#     for k in n:
+#         print(k)                        # line by line
+
+
+''' signup login '''
+
+# username = input("Enter username :")
+# password = input("Enter password :")
+# print("User created")
+# gess = input("Enter username :")
+# password = input("Enter password :")
+# if (username==gess and password ==password):
+#     print("Login successfull")
+# else:
+#     print("Invalid credentials")
+
+
+""" calling the def function on another file """
+
+# import hell
+
+# hell.hug()                                      # You are now mine
+# hell.juk()                                      # You must be available on here
+
+# import tenambatch as tb
+
+# tb.day()                                # tomorrow there is no class
+# tb.clg()                                # Today we need you
+
+
+''' class object'''
+
+# class python():
+#     course1 = "Cyber security"
+#     course2 = "AI"
+# print(python.course1)                               # Cyber security
+# print(python.course2)                               # AI
+# print(python.course2, python.course1)               # AI Cyber security
+# c= python()
+# print(c.course1)                                    # Cyber security
+# print(c.course2)                                    # AI
+# print(c.course1,c.course2)                          # Cyber security AI
+
